bdabdaeafd9d5533bb72b39b50373f1a688b59d6
Anusha Balusu <anusha.balusu08@gmail.com>
2016-03-14 02:16:31 -0400

    Create PlayerViewInterface.java

---
+++ RMI/Connect4/PlayerViewInterface.java
@@ -0,0 +1,12 @@
+/**
+ * Interface for the Player view
+ *
+ * @version    $Id: PlayerViewInterface.java, v 1 2015/05/10 $
+ *
+ * @author     Anusha Balusu, Pankhuri Roy
+ **/
+
+public interface PlayerViewInterface {
+   
+   public int nextMove(PlayerModelInterface p);
+}
